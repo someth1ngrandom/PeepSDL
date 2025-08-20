6869f0f252d54d4a3389cd5a9b53450a4b9592bb
something <oursweetass@gmail.com>
2025-08-20 22:10:59 +0300

    remove gamestate, better window constructor, examples

---
+++ samples/basic_window/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <class/Window.h> // for Peep::Window
+#include <class/Color.h> // for Peep::Color
+
+int main() {
+
+    Peep::Window window("PeepSDL Sample Window", 800, 600); // create an 800x600 SDL window
+    // for fullscreen, you can do this:
+    // Peep::Window window("PeepSDL Sample Window", 1 /* <-- change this to 2 for borderless */);
+
+    window.SetLogicLoop([](){});   // define an empty logic and draw loop,
+    window.SetDrawLoop([](){});    // we are not doing anything yet in this sample
+
+    window.BeginLoop(Peep::Color::hex(0x2A2A2A)); // start our empty loops and fill the background with a dark grey color
+
+    SDL_Quit();
+    return 0;
+}
--- src/class/Window.cpp
+++ src/class/Window.cpp
@@ -1,31 +1,63 @@
 #include "class/Window.h"
-#include <format>
-#include "class/Gamestate.h"
 #include <SDL3_ttf/SDL_ttf.h>
 
 namespace Peep {
 
 bool Window::running = true;
 
-Window::Window(std::string_view title, int *width, int *height, SDL_WindowFlags flags, GamepadMap &gamepads) {
+Window::Window(std::string_view title, int fullscreenLevel) {
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_GAMEPAD);
 
-    int realWidth = 1600;
-    int realHeight = 900;
+    int w = 640;
+    int h = 480;
+    SDL_WindowFlags flags = 0;
 
-    SDL_DisplayID displayId = SDL_GetPrimaryDisplay();
-    const SDL_DisplayMode* mode = SDL_GetCurrentDisplayMode(displayId);
-    if (mode) {
-        realWidth = mode->w;
-        realHeight = mode->h;
-    } else {
-        // we fall back to the default resolution
+    if (fullscreenLevel > 0) {
+        SDL_DisplayID displayId = SDL_GetPrimaryDisplay();
+        const SDL_DisplayMode* mode = SDL_GetCurrentDisplayMode(displayId);
+        if (mode) {
+            w = mode->w;
+            h = mode->h;
+        } else {
+            throw std::runtime_error(SDL_GetError());
+        }
     }
 
-    if (width) *width = realWidth;
-    if (height) *height = realHeight;
+    if (fullscreenLevel == 0) {
+        flags |= SDL_WINDOW_FULLSCREEN;
+    } else if (fullscreenLevel == 1) {
+        flags |= SDL_WINDOW_BORDERLESS;
+    }
+ 
+    window = SDL_CreateWindow(title.data(), w, h, flags);
+    if (!window) {
+        throw std::runtime_error(SDL_GetError());
+    }
 
-    window = SDL_CreateWindow(title.data(), realWidth, realHeight, flags);
+    SDL_SetWindowResizable(window, false);
+
+    renderer = SDL_CreateRenderer(window, nullptr);
+    if (!renderer) {
+        throw std::runtime_error(SDL_GetError());
+    }
+
+    SDL_SetWindowFullscreen(window, true);
+    SDL_SetWindowFullscreenMode(window, NULL); 
+
+
+    // set vsync
+    SDL_SetRenderVSync(renderer, 1);
+
+    SDL_SetRenderDrawBlendMode(renderer, SDL_BLENDMODE_BLEND); // enable blending, allows for alpha
+
+    lastPerfCounter = SDL_GetPerformanceCounter();
+    perfFreq = SDL_GetPerformanceFrequency();
+}
+
+Window::Window(std::string_view title, int width, int height, SDL_WindowFlags flags) {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK | SDL_INIT_GAMEPAD);
+
+    window = SDL_CreateWindow(title.data(), width, height, flags);
     if (!window) {
         throw std::runtime_error(SDL_GetError());
     }
@@ -114,7 +146,6 @@ void Window::InitMix(MIX_InitFlags flags/* = MIX_INIT_OGG*/) {
     Mix_Volume(-1, MIX_MAX_VOLUME);
 }
 
-int Window::GetJoystickCount(Gamestate &state) const noexcept { return state.gamepads.size(); }
 void Window::Clear() { SDL_RenderClear(renderer); }
 void Window::Present() { SDL_RenderPresent(renderer); }
 void Window::SetDrawColor(Color c) { SDL_SetRenderDrawColor(renderer, c.r, c.g, c.b, c.a); }
